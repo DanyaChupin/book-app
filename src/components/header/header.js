408ea87448ee7b1bb29a41c95a077c6abd927e88
Данил Чупин <chupa.chupa.14@mail.ru>
2023-10-05 16:25:57 +0300

    final header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import { DivComponent } from '../../common/div-component'
+import './header.css'
 
 export class Header extends DivComponent {
 	constructor(appState) {
@@ -11,6 +12,19 @@ export class Header extends DivComponent {
 		this.el.innerHTML = `
 			<div>
 			<img src="/static/logo.svg" alt='Logo'/>
+			</div>
+			<div class='menu'>
+				<a class="menu__item" href='#'>
+					<img src="/static/search.svg"/>
+					Search Books
+				</a>
+        <a class="menu__item" href='#favorites'>
+					<img src="/static/favorites.svg"/>
+				Favorites	
+				<div class="menu__counter">
+					${this.appState.favorites.length}
+				</div>
+				</a>
 			</div>
 		`
 		return this.el
