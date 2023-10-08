c10111a673708a506bfcbb1d0fdabd7c951f7be4
Данил Чупин <chupa.chupa.14@mail.ru>
2023-10-08 19:33:36 +0300

    Компонент карты, галлереи.

---
+++ src/components/card/card.js
@@ -0,0 +1,45 @@
+import { DivComponent } from '../../common/div-component'
+import './card.css'
+
+export class Card extends DivComponent {
+	constructor(appState, cardState) {
+		super()
+		this.appState = appState
+		this.cardState = cardState
+	}
+
+	render() {
+		this.el.classList.add('card')
+		const existInFavorites = this.appState.favorites.find(
+			b => b.key == this.cardState.key
+		)
+		this.el.innerHTML = `
+			<div class='card__image'>
+				<img src='https://covers.openlibrary.org/b/olid/${
+					this.cardState.cover_edition_key
+				}-M.jpg' alt='cover'/>
+			</div>
+			<div class='card__info'>
+					<div class='card__tag'>
+						${this.cardState.subject ? this.cardState.subject[0] : 'Not set'}
+					</div>
+					<div class='card__name'>
+						${this.cardState.title}
+					</div>
+					<div class='card__author'>
+						${this.cardState.author_name ? this.cardState.author_name[0] : 'Not set'}
+					</div>
+					<div class='card__footer'>
+					<button class='button__add ${existInFavorites ? 'button__active' : ''}'>
+						${
+							existInFavorites
+								? '<img src="/static/favoritesblack.svg"/>'
+								: '<img src="/static/favoritesWhite.svg"/>'
+						}
+					</button>
+				</div>
+			</div>
+		`
+		return this.el
+	}
+}
